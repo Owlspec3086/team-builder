83c1e913e3d489c87d071afa14cc6723295f3446
Owlspec3086 <ethical.worker3@gmail.com>
2020-05-19 22:45:34 -0400

    Flex assignment forms

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {v4 as uuid } from 'uuid';
 import './App.css';
 
+import TeamMemberForm from './component/TeamMemeberForm';
+import TeamMember from './component/TeamMember';
+
+// Creatind a Teamlist
 function App() {
   return (
     <div className="App">
